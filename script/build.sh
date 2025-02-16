265711c718c36aaf444c7838c6300ba33fdc883f
andersonvendramin <vendramin2k18@gmail.com>
2025-02-16 10:34:35 -0400

    finish hello world

--- script/build.sh
+++ script/build.sh
@@ -3,5 +3,8 @@
 cd "$(dirname "$0")" || exit
 cd ../build
 
-nasm -f elf64 ../source/nasm/hello_world.asm -o hello_world.o
+# nasm -f elf64 ../source/nasm/disassembler/disassembler.asm -o disassembler.o
+# ld -o disassembler -e main disassembler.o
+
+nasm -f elf64 ../source/x64/nasm/hello_world.asm -o hello_world.o
 ld -o hello_world -e main hello_world.o
---
+++ source/x64/nasm/hello_world.asm
@@ -0,0 +1,47 @@
+section .data
+    STACK_SIZE equ 32
+    SYS_WRITE equ 1
+    SYS_EXIT equ 60
+
+    message db "Hello, world!", 10
+    message_length equ $ - message
+
+section .text
+    global main
+
+write_console:
+    ; parameters: string, string_length
+    ; variables: string, string_length
+
+    ; preamble
+    push rbp
+    mov rbp, rsp
+    sub rsp, STACK_SIZE
+
+    ; variables
+    mov [rsp], rdi
+    mov [rsp + 8], rsi
+
+    ; write to console the string x bytes
+    mov rdi, 1
+    mov rsi, [rsp]
+    mov rdx, [rsp + 8]
+    mov rax, SYS_WRITE
+    syscall
+
+    ; postable
+    add rsp, STACK_SIZE
+    pop rbp
+    ret
+
+main:
+    ; writes message to the standard output
+    mov rdi, message
+    mov rsi, message_length
+    call write_console
+
+    ; exits the program 
+    xor rdi, rdi
+    mov rax, SYS_EXIT
+    syscall
+    ret
