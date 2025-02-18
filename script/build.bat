640aa0a7d6ab4c65d9da3c1d77b8b2e011327dc6
andersonvendramin <vendramin2k18@gmail.com>
2025-02-18 17:38:17 -0400

    delete almost everything in favor of a bad idea

--- script/build.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-CD %~dp0
-IF NOT EXIST "..\build" (MD "..\build")
-CD ..\build
-nasm -f win64 ..\source\x64\nasm\hello_world.asm -o .\hello_world.o
-ld .\hello_world.o -o .\hello_world.exe -e main -luser32 -l kernel32
-CD %~dp0
